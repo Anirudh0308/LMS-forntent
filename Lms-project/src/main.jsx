aa82984fb8f2029bb4ca4dc04e2de80d6b6b1c7d
Anirudh0308 <anirudhgpj360@gmail.com>
2025-05-11 20:29:41 +0530

    add slice Authslice

--- Lms-project/src/main.jsx
+++ Lms-project/src/main.jsx
@@ -1,11 +1,19 @@
-import { createRoot } from 'react-dom/client'
 import './index.css'
+
+import ReactDom from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
+import { Toaster } from 'react-hot-toast'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+
 import App from './App.jsx'
+import store from './Redux/store.js'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-    <Toster />
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+      <Toaster />
     </BrowserRouter>
-  
+  </Provider>
 )
